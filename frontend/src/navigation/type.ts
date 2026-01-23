b97829efedb8d91c52516c0c8a591d7be280c33f
ChoInHeum <heumi711@gmail.com>
2026-01-23 22:38:28 +0900

    [setting] frontend init

---
+++ frontend/src/navigation/type.ts
@@ -0,0 +1,10 @@
+import { NavigatorScreenParams } from '@react-navigation/native';
+
+export type RootStackParamList = {
+  Home: undefined;
+  AuthNavigator: NavigatorScreenParams<AuthStackParamList>;
+};
+
+export type AuthStackParamList = {
+  Auth: undefined;
+};
